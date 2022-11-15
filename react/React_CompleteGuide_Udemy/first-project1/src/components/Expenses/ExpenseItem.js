2d10461953c8fca98ddc4aed64135bd60e100430
Mokdasoo <ahrektn@gmail.com>
2022-11-16 02:36:09 +0900

    section4 리액트 State 및 이벤트 다루기 완료

--- react/React_CompleteGuide_Udemy/first-project1/src/components/Expenses/ExpenseItem.js
+++ react/React_CompleteGuide_Udemy/first-project1/src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,9 @@ import ExpenseDate from "./ExpenseDate";
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
+
 const ExpenseItem = (props) => {
+
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
@@ -10,6 +12,8 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+
+            
         </Card>
     );
 }
