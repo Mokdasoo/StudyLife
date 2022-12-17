cd7d07dac3b149f5224e46c70b6666cce293b31e
Mokdasoo <ahrektn@gmail.com>
2022-12-17 17:53:18 +0900

    react-native/locationPicker ui

--- react-native/React-Native_CompleteGuide_Udemy/favorite-places/components/Places/ImagePicker.js
+++ react-native/React-Native_CompleteGuide_Udemy/favorite-places/components/Places/ImagePicker.js
@@ -1,44 +1,77 @@
-import { Alert, Button, View } from "react-native";
-import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
+import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { useState } from "react";
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 const ImagePicker = () => {
-    const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
+    const [pickedImage, setPickedImage] = useState(null);
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
 
-    const verifyPermissions = async () => {
-        if(cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
-            const permissionResponse = await requestPermission();
+  const verifyPermissions = async () => {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
 
-            return permissionResponse.granted;
-        }
-
-        if(cameraPermissionInformation.status === PermissionStatus.DENIED) {
-            Alert.alert('Insufficient Permissions!', 'You need to grant camera permissions to use this app');
-            return false;
-        }
-
-        return true;
+      return permissionResponse.granted;
     }
 
-    const takeImageHandler = async () => {
-        const hasPermission = await verifyPermissions();
-        if(!hasPermission){
-            return;
-        }
-        const image = await launchCameraAsync({
-            allowsEditing: true,
-            aspect: [16,9],
-            quality: 0.5,
-        });
-        console.log(image);
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to use this app"
+      );
+      return false;
     }
-    return (
-        <View>
-            <View>
 
-            </View>
-            <Button title="Take Image" onPress={takeImageHandler} />
-        </View>
-    );
+    return true;
+  };
+
+  const takeImageHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    setPickedImage(image.assets[0].uri);
+  };
+  let imagePreview = <Text>No image taken yet.</Text>
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{uri: pickedImage}} />;
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>
+        {imagePreview}
+      </View>
+      <OutlinedButton icon='camera' onPress={takeImageHandler}>Take Image</OutlinedButton>
+    </View>
+  );
 };
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: '100%',
+        height: 200,
+        marginVertical: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.primary100,
+        borderRadius: 4
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+});
